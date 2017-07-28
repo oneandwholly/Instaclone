08678137f02c738435fde22baf3b1ae1a6b961b8
Jon Huh <oneandwholly@gmail.com>
2017-07-28 19:14:16 +0900

    add starter files

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import app from './modules/app';
+
+const App = app.components.App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
